2b230c96bd370952f53dbc5acf7f05ea3af8b4c6
Myrsini Stasinou <myrsinistas@gmail.com>
2025-05-03 00:46:13 +0300

    added jLayer Library

--- src/main/java/io/github/myrsstas/musicplayer/controllers/MusicPlayerControllerImpl.java
+++ src/main/java/io/github/myrsstas/musicplayer/controllers/MusicPlayerControllerImpl.java
@@ -1,12 +1,23 @@
 package io.github.myrsstas.musicplayer.controllers;
 
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 public class MusicPlayerControllerImpl implements MusicPlayerController {
-
-
     @Override
     public void playSong(String songPath) {
-
+        File file = new File(songPath);
+        try{
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            Thread.sleep(clip.getMicrosecondLength()/1000);
+        } catch (UnsupportedAudioFileException | IOException |LineUnavailableException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- src/main/java/io/github/myrsstas/musicplayer/controllers/MusicScannerControllerImpl.java
+++ src/main/java/io/github/myrsstas/musicplayer/controllers/MusicScannerControllerImpl.java
@@ -15,7 +15,8 @@ public class MusicScannerControllerImpl implements MusicScannerController {
 
     public MusicScannerControllerImpl(FileScannerController fileScannerController) {
         this.fileScannerController = fileScannerController;
-        this.musicExtensions = Arrays.asList("mp3", "aiff", "wav");
+        this.musicExtensions = Arrays.asList("mp3", "aac", "wav");
+//        this.musicExtensions = Arrays.asList("wav");
     }
 
     @Override
